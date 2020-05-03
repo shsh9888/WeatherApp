6ead128af04a0d452b5796d972463fa804732bde
Shravan Shetty <shsh9888@colorado.edu>
2020-05-02 20:54:09 -0600

    Hide the keyboard on button click

--- app/src/main/java/com/example/weatherapp/MainActivity.java
+++ app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -3,8 +3,10 @@ package com.example.weatherapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -23,6 +25,8 @@ public class MainActivity extends AppCompatActivity {
         try {
             EditText cityView = (EditText) findViewById(R.id.cityEntered);
             String city = cityView.getText().toString();
+            InputMethodManager mgr = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+            mgr.hideSoftInputFromWindow(cityView.getWindowToken(), 0);
             new CurrentWeather(this).execute(city).get();
         } catch (ExecutionException e) {
             e.printStackTrace();
